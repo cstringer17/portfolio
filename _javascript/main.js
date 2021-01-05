1afc476b95f4bb4cfe81e2ef5fbd405af87e69b5
cstringer17 <callumstringer10@gmail.com>
2021-01-05 16:43:24 +0100

    added console with basic functionality

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
+
+
+
+
+
+$("#console-input").on('keyup', function (e) {
+  if (e.key === 'Enter' || e.keyCode === 13) {
+    $("#console-output").append($("#console-input").val() + "&#13;&#10;");
+    $("#console-input").val("src&#92;portfolio>");
+
+    var psconsole = $('#console-output');
+    if (psconsole.length)
+      psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());
+  }
+});
