a3cf806f1eb1d89634b565fd4be386e10263bb92
callum stringer <callumstringer10@gmail.com>
2021-01-07 17:50:29 +0100

    hoppity bobbity jquery is not my property

--- _javascript/console-commands.js
+++ _javascript/console-commands.js
@@ -1,14 +1,15 @@
 
+var newLine = String.fromCharCode(13, 10);
+
 function getCommands() {
     var commands = {
         "-list": "Lists something like repos",
         "-help": "Lists all commands"
     };
-    $("#console-output").append("These are the available commands" + "&#13;&#10;");
 
+    document.querySelector('#console-output').append("These are the available commands" + newLine);
     for (const [key, value] of Object.entries(commands)) {
-
-        $("#console-output").append(`${key}: ${value}` + "&#13;&#10;");
+        document.querySelector('#console-output').append(`${key}: ${value}` + newLine);
     }
     return
 }
@@ -29,16 +30,13 @@ function list(parameters) {
 
 function getRepos() {
     var reply = "Repos that I have made:"
-
-    $(document).ready(function () {
-        $.ajax({
-            url: "https://api.github.com/users/cstringer17/repos"
-        }).then(function (data) {
-            console.log(data);
-            data.forEach(element => {
-                console.log(element.name);
-                $("#console-output").append(element.name + "&#13;&#10;");
-            });
+    
+    fetch('https://api.github.com/users/cstringer17/repos')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            console.log(element.name);
+            document.querySelector('#console-output').append(element.name + newLine);
         });
     });
     return reply
